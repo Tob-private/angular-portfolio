7907541acd2482e1950c68e8d3ef2a05bbdd8f02
Tobias Riiser <tobias.riiser.games@gmail.com>
2024-07-01 19:29:56 +0200

    added data and styling to projects

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../../services/header.service';
+import { ProjectsService } from '../../services/projects.service';
+import { ProjectType } from '../../types/project.types';
+import { ProjectCardComponent } from '../project-card/project-card.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [ProjectCardComponent, NgFor],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
-export class ProjectsComponent {
 
+export class ProjectsComponent implements OnInit {
+  projects: Array<ProjectType> = []
+  constructor(public headerService: HeaderService, public projectsService: ProjectsService) {
+    this.headerService.setHeaderText("My Project Showcase")
+  }
+
+  async ngOnInit() {
+    await this.projectsService.fetchProjects()
+    this.projects = this.projectsService.projects
+
+
+
+  }
 }
