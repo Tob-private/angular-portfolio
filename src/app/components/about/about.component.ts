7715e30ba11535d96b2212f142fe846d612cdac8
Tobias Riiser <tobias.riiser.games@gmail.com>
2024-07-01 10:46:33 +0200

    added text and header to about me page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HeaderService } from '../../services/header.service';
 
 @Component({
   selector: 'app-about',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+  constructor(public headerService: HeaderService) {
+    this.headerService.setHeaderText('Me and my story');
+  }
 }
